1e4127345d1ebcc04ed1e152e3841f527593ea9d
Daniel <d.spinardi199@gmail.com>
2021-04-17 20:36:13 -0300

    Add facade method to get the words from the word list in array

--- src/ConsumerFacade.php
+++ src/ConsumerFacade.php
@@ -4,4 +4,17 @@ namespace WordlistConsumer;
 
 class ConsumerFacade
 {
+    /**
+     * @var array
+     */
+    private static $wordlist;
+
+    /**
+     * @param string $path
+     * @return void
+     */
+    public static function make(string $path)
+    {
+        self::$wordlist = (new Consumer($path))->open();
+    }
 }
