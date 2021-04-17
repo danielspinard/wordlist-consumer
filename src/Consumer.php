f42754e0d7ea8621e1a522363b6ca3f93701d5ae
Daniel <d.spinardi199@gmail.com>
2021-04-17 20:09:50 -0300

    Add Consumer handler class

---
+++ src/Consumer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WordlistConsumer;
+
+use WordlistConsumer\Exceptions\WordlistNotFoundException;
+
+class Consumer
+{
+    /**
+     * @var string
+     */
+    private static $path;
+}
