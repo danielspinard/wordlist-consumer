630cacd4bd0cdb37165978825390747ab66b1ee7
Daniel <d.spinardi199@gmail.com>
2021-04-18 01:03:35 -0300

    Add methods to Consumer Interface

--- src/Interfaces/ConsumerInterface.php
+++ src/Interfaces/ConsumerInterface.php
@@ -2,6 +2,32 @@
 
 namespace WordlistConsumer\Interfaces;
 
+use WordlistConsumer\ConsumerOptions;
+
 interface ConsumerInterface
 {
+    /**
+     * @param ConsumerOptions $option
+     */
+    public function __construct(ConsumerOptions $option = null);
+
+    /**
+     * @param string $attribute
+     * @param mixed $value
+     * @return ConsumerInterface
+     */
+    public function __set(string $attribute, $value): ConsumerInterface;
+
+    /**
+     * @param string $attribute
+     * @param mixed $value
+     * @return mixed
+     */
+    public function __get(string $attribute);
+
+    /**
+     * @param string $attribute
+     * @return bool
+     */
+    public function __isset(string $attribute): bool;
 }
