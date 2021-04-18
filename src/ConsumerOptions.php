7b5651aef83ced21777333ac79fedd224f0bf7a9
Daniel <d.spinardi199@gmail.com>
2021-04-18 01:10:29 -0300

    Add method to find specific option

--- src/ConsumerOptions.php
+++ src/ConsumerOptions.php
@@ -11,4 +11,19 @@ class ConsumerOptions
      * @var array
      */
     private static $options;
+
+    /**
+     * @param string $name
+     * @return array|null
+     */
+    private static function find(string $name): ?array
+    {
+        foreach (self::getOptions() as $option) {
+            if ($name === key($option)) {
+                return $option;
+            }
+        }
+
+        return null;
+    }
 }
