1a65b9a25f3e74018fc00a350b8ca3600edae663
danielspinard <d.spinardi199@gmail.com>
2021-05-22 21:09:54 -0300

    Preparing AddressConsumer

---
+++ src/Consumers/AddressConsumer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WordlistConsumer\Consumers;
+
+class AddressConsumer
+{
+    /**
+     * @var string
+     */
+    protected $city;
+
+    /**
+     * @var string
+     */
+    protected $country;
+
+    /**
+     * @var string
+     */
+    protected $state;
+
+    /**
+     * @var int
+     */
+    protected $geoNameId;
+}
