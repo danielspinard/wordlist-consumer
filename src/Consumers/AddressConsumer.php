1c3ad0c065e3babedf654f5528815895ca3d79ca
danielspinard <d.spinardi199@gmail.com>
2021-05-22 21:11:48 -0300

    Implement ConsumerInterface methods

--- src/Consumers/AddressConsumer.php
+++ src/Consumers/AddressConsumer.php
@@ -2,7 +2,11 @@
 
 namespace WordlistConsumer\Consumers;
 
-class AddressConsumer
+use WordlistConsumer\Interfaces\ConsumerInterface;
+use WordlistConsumer\Interfaces\ConsumerMakerInterface;
+use WordlistConsumer\ConsumerOptions;
+
+class AddressConsumer implements ConsumerInterface
 {
     /**
      * @var string
@@ -23,4 +27,59 @@ class AddressConsumer
      * @var int
      */
     protected $geoNameId;
+
+    /**
+     * AddressConsumer constructor
+     * 
+     * @param ConsumerOptions $option
+     */
+    public function __construct(ConsumerOptions $consumerOptions = null)
+    {
+    }
+
+    /**
+     * @param string $attribute
+     * @param mixed $value
+     * @return ConsumerInterface
+     */
+    public function __set(string $attribute, $value): ConsumerInterface
+    {
+        $this->$attribute = $value;
+        return $this;
+    }
+
+    /**
+     * @param string $attribute
+     * @param mixed $value
+     * @return mixed
+     */
+    public function __get(string $attribute)
+    {
+        return ($this->__isset($attribute) ? $this->$attribute : null);
+    }
+
+    /**
+     * @param string $attribute
+     * @param mixed $value
+     * @return bool
+     */
+    public function __isset(string $attribute): bool
+    {
+        return isset($this->$attribute);
+    }
+
+    /**
+     * @param ConsumerMakerInterface $maker
+     * @return ConsumerInterface
+     */
+    public function __make(ConsumerMakerInterface $maker): ConsumerInterface
+    {
+        $person = $maker->result();
+
+        foreach ($person as $attribute => $value) {
+            $this->__set($attribute, $value);
+        }
+
+        return $this;
+    }
 }
